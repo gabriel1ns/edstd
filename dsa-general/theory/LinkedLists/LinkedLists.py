18fa9cc375225e020022d19d496d797ac50f92d6
Gabriel Lins <gabriellinsalvesdonascimento@gmail.com>
2025-09-09 10:39:39 -0300

    add append method

--- dsa-general/theory/LinkedLists/LinkedLists.py
+++ dsa-general/theory/LinkedLists/LinkedLists.py
@@ -19,5 +19,24 @@ class LinkedList:
             print(temp.value)
             temp = temp.next
 
-my_ll = LinkedList(4);
-print(my_ll.head.value)
+    def append(self,value):
+        new_node = Node(value)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+
+        else:
+            self.tail.next = new_node #we need that what comes after the last item (tail) to stop pointing to None and point to new_node
+            self.tail = new_node
+        self.lenght += 1
+        return True #For future reference.
+        
+
+my_linked_list = LinkedList(1);
+my_linked_list.printList()
+
+
+my_linked_list.append(2)
+my_linked_list.printList()
+
