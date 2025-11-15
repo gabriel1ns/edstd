2440b4514ee142b3264765f56b50a7ed95b1acfd
Gabriel Lins <gabriellinsalvesdonascimento@gmail.com>
2025-11-15 11:08:28 -0300

    questao 4 - lista 3

---
+++ dsa-poli/lista3/Questão4.py
@@ -0,0 +1,27 @@
+def hasPair(list, sum):
+    if len(list) < 2:
+        return None
+    
+    sorted_list = sorted(list)
+
+    left  = 0
+    right = len(sorted_list - 1)
+
+    while left < right:
+        current_sum = sorted_list[left] + sorted_list[right]
+
+        if current_sum == sum:
+            return True, (sorted_list[left] + sorted_list[right])
+        elif current_sum < sum:
+            esquerda +=1
+        else:
+            direita -=1
+        
+    return False
+
+'''
+O método hasPair() vai combinar a ordenação e a varredura da lista com os ponteiros left e right
+Dessa forma é possivel escapar da complexidade O(n^2), que é a da solução clássica desse tipo de problema usando nested loops
+Esse algoritmo possui complexidade O(nlog(n)), já que criamos a lista ordenada (log(n))
+e varemos ela toda em busca da soma destino: sum.
+'''
