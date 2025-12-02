0b12c117af0b08837705969adab2845f084bc33e
Gabriel Lins <gabriellinsalvesdonascimento@gmail.com>
2025-12-02 15:44:11 -0300

    add questao1 e questao2 - lista 5

---
+++ dsa-poli/lista5/questão2.py
@@ -0,0 +1,46 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+def verify_binary_tree(root):
+    if root is None:
+        return True
+    
+    visited = {}
+    
+    def dfs(node, parent=None):
+        node_id = id(node)
+        if node_id in visited:
+            return False
+        
+        visited[node_id] = True
+        
+        children = 0
+        if node.left is not None:
+            children += 1
+        if node.right is not None:
+            children += 1
+        
+        if children > 2:
+            return False
+        
+        if node.left is not None:
+            valid, msg = dfs(node.left, node)
+            if not valid:
+                return False, msg
+        
+        if node.right is not None:
+            valid, msg = dfs(node.right, node)
+            if not valid:
+                return False, msg
+        
+        return True, ""
+    
+    valid, message = dfs(root)
+    
+    if valid:
+        return True
+    else:
+        return False
